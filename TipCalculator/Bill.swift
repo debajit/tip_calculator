1d23871e376fe8b773031705b23647063d70ee95
Debajit Adhikary <debajit@debajit.com>
2017-08-06 12:50:12 -0700

    Add Bill business logic

---
+++ TipCalculator/Bill.swift
@@ -0,0 +1,31 @@
+//
+//  Bill.swift
+//  TipCalculator
+//
+//  Business logic for a bill with a tip. Implements core logic for 
+//  bill-with-tip calculations, tip percentages offered etc.
+//
+//  Created by Debajit Adhikary on 8/6/17.
+//  Copyright © 2017 Debajit Adhikary. All rights reserved.
+//
+
+class Bill {
+    static let tipPercentages: [Double] = [15, 20, 25]
+    
+    var amount: Double = 0
+    var tipIndex: Int = 0
+    
+    init(amount: Double, tipIndex: Int) {
+        self.amount = amount
+        self.tipIndex = tipIndex
+    }
+    
+    var tipAmount: Double {
+        let tipPercentage = Bill.tipPercentages[tipIndex]
+        return amount * tipPercentage / 100
+    }
+    
+    var amountWithTip: Double {
+        return amount + tipAmount
+    }
+}
